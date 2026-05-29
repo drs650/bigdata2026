783f04b7e2ed8d763ac959a2ef8aa7ba2b176ef4
drs650 <huborobot@gmail.com>
2026-05-29 16:10:53 +0900

    260529

---
+++ 260529/31_.py
@@ -0,0 +1,10 @@
+def sub(a: int, b: int) -> int:
+    return a - b
+
+result = sub(20, 10) # 함수 호출
+print(result)
+print(type(result))
+
+result2 = sub(20.2, 10.1)
+print(result2)
+print(type(result2))
